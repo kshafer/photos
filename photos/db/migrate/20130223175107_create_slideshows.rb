2047134a90790a9c163aff35296e1eb11fd4456b
KC <kc@ubuntu.(none)>
2013-02-23 16:53:17 -0500

    added sql install script, first round of database migration scripts, scaffold controller and active record controllers.

---
+++ photos/db/migrate/20130223175107_create_slideshows.rb
@@ -0,0 +1,13 @@
+class CreateSlideshows < ActiveRecord::Migration
+  def self.up
+    create_table :slideshows do |t|
+        t.column "name", :string
+        t.column "created_at", :datetime
+    end
+  end
+
+  def self.down
+    drop_table "slideshows"
+  end
+
+end
---
+++ photos/db/migrate/20130223175131_create_slides.rb
@@ -0,0 +1,14 @@
+class CreateSlides < ActiveRecord::Migration
+  def change
+    create_table :slides do |t|
+        t.column "position", :integer
+        t.column "photo_id", :integer
+        t.column "slideshow_id", :integer
+    end
+  end
+
+  def self.down
+    drop_table "slides"
+  end
+
+end
