f2674bdfa4a8e7375509d984ad38a0fde066fd1c
KC <kc@ubuntu.(none)>
2013-02-23 11:30:39 -0500

    initial commit

---
+++ photos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photos::Application.config.secret_token = 'da6e7dacd5e5dd21f5fcf499aca197e01a817d39b232a3a84eb757a70ebd5f910eec5eb8154ff740dece2d799f916b1e5382b27ba3fdff4ca5b7af7eae70687e'
